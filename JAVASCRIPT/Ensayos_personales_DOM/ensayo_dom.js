da64f7a388409d2a34eda85b70c27802a31c6cb2
ookami9029 <moncadamontoya@gmail.com>
2026-06-18 11:48:36 -0500

    trabajo de numeros aleatorios

--- JAVASCRIPT/Ensayos_personales_DOM/ensayo_dom.js
+++
@@ -1,9 +0,0 @@
-const titulo=document.getElementById("titulo");
-const resultado=document.getElementById("resultado");
-const boton=document.getElementById("btn");
-boton.addEventListener ('mouseover', () => {
-    boton.style.backgroundColor='lightgreen';
-})
-boton.addEventListener('mouseout', ()=>{
-    boton.style.backgroundColor='';
-})
